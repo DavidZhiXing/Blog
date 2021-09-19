5a8fcbd1c432375f92747ff5f4ddc0314aa6e415
weizhixing <2591612720@qq.com>
2021-09-19 16:38:48 +0800

    arrow function

--- 30-seconds/SumAnObjectArray.js
+++ 30-seconds/SumAnObjectArray.js
@@ -1,8 +1,6 @@
 let init = 0;
 let sum = [{x: 1}, {x: 2}, {x: 3}].reduce(
-    function (pre, current) {
-        return pre + current.x;
-    },
+     (pre, current) =>pre + current.x,
     init
 )
 
