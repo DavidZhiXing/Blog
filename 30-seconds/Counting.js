0ee1595c198b9cbdd3c5d2ebe7a30d2b1e7f2b21
weizhixing <2591612720@qq.com>
2021-09-19 18:00:02 +0800

    group by

--- 30-seconds/Counting.js
+++ 30-seconds/Counting.js
@@ -12,3 +12,22 @@ let count = names.reduce(
 )
 
 console.log(count);
+
+let people = [
+    {name: 'Alice', age: 21},
+    { name: 'Max', age: 20},
+    { name: 'Jane', age: 20}
+];
+
+const groudby = (obj, property) => obj.reduce(
+    (acc, item) => {
+        let key = item[property];
+        if (!acc[key]) {
+            acc[key] = []
+        }
+        acc[key].push(item)
+        return acc
+    }, {}
+)
+
+console.log(groudby(people, 'age'))
