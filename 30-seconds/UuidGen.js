16b4bf36ac11a6100ded7679f7720482575ede85
weizhixing <2591612720@qq.com>
2021-09-21 11:58:54 +0800

    uuid generator via node

--- 30-seconds/UuidGen.js
+++ 30-seconds/UuidGen.js
@@ -1,7 +1,9 @@
+const crypto = require('crypto')
+
 const UuidGenBrowser = () =>
     ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c=>
         (
-           c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c/4))) 
+           c ^ (crypto.randomBytes(1)[0] & (15 >> (c/4))) 
         ).toString(16));
 
 console.log(UuidGenBrowser())
