dc84406043c9946b5980e6dc52b0d7ceaa80a36c
weizhixing <2591612720@qq.com>
2021-10-19 09:22:35 +0800

    [LinkedList]: C Sharp

---
+++ algorithms/LinkedLIst.cs
@@ -0,0 +1,226 @@
+public sealed class LinkedListNode<T>{
+    internal LinkedList<T> list;
+    internal LinkedListNode<T> next;
+    internal LinkedListNode<T> prev;
+    internal T item;
+
+    public LinkedListNode(T value) {
+        item = value;
+    }
+
+    public LinkedListNode(LinkedList list, T value) {
+        this.list = list;
+        item = value;
+    }
+
+    public LinkedList<T> List {
+        get{return list;}
+    }
+
+    public LinkedListNode<T> Next {
+        get{return next == null || next == list.head ? null:next；}
+    }
+
+    public LinkedListNode<T> Prev {
+        get{return prev == null || this == list.head ? null:prev;}
+    }
+
+    public T Value{
+        get{ return item;}
+        set{ item = value;}
+    }
+
+    internal void Invalid(){
+        list = null;
+        next = null;
+        prev = null;
+    }
+
+    public class LinkedList<T> : ICollection<T>, ICollection, IReadOnlyCollection<T>, ISerializable, IDeserializationCallback {
+        internal LinkedListNode<T> head;
+        internal int count;
+        internal int version;
+
+        public LinkedList(IEnumerable<T> collection) {
+            foreac(T item in collection) {
+                AddLast(item);
+            }
+        }
+
+        public LinkedListNode<T> First
+        {
+            get { return head; }
+        }
+
+        public LinkedListNode<T> Last
+        {
+            get { return head == null ? null : head.prev; }
+        }
+
+
+        public void AddAfter(LinkedListNode<T> node, LinkedListNode<T> newNode)
+        {
+            ValidateNode(node);
+            ValidateNewNode(newNode);
+            InternalInsertNodeBefore(node.next, newNode);
+            newNode.list = this;
+        }
+
+        public LinkedListNode<T> AddBefore(LinkedListNode<T> node, T value)
+        {
+            ValidateNode(node);
+            LinkedListNode<T> result = new LinkedListNode<T>(node.list, value);
+            InternalInsertNodeBefore(node, result);
+            if (node == head)
+            {
+                head = result;
+            }
+            return result;
+        }
+
+        public void AddBefore(LinkedListNode<T> node, LinkedListNode<T> newNode)
+        {
+            ValidateNode(node);
+            ValidateNewNode(newNode);
+            InternalInsertNodeBefore(node, newNode);
+            newNode.list = this;
+            if (node == head)
+            {
+                head = newNode;
+            }
+        }
+
+        public LinkedListNode<T> AddFirst(T value)
+        {
+            LinkedListNode<T> result = new LinkedListNode<T>(this, value);
+            if (head == null)
+            {
+                InternalInsertNodeToEmptyList(result);
+            }
+            else
+            {
+                InternalInsertNodeBefore(head, result);
+                head = result;
+            }
+            return result;
+        }
+
+        public void AddFirst(LinkedListNode<T> node)
+        {
+            ValidateNewNode(node);
+
+            if (head == null)
+            {
+                InternalInsertNodeToEmptyList(node);
+            }
+            else
+            {
+                InternalInsertNodeBefore(head, node);
+                head = node;
+            }
+            node.list = this;
+        }
+
+        public LinkedListNode<T> AddLast(T value)
+        {
+            LinkedListNode<T> result = new LinkedListNode<T>(this, value);
+            if (head == null)
+            {
+                InternalInsertNodeToEmptyList(result);
+            }
+            else
+            {
+                InternalInsertNodeBefore(head, result);
+            }
+            return result;
+        }
+
+        public void AddLast(LinkedListNode<T> node)
+        {
+            ValidateNewNode(node);
+
+            if (head == null)
+            {
+                InternalInsertNodeToEmptyList(node);
+            }
+            else
+            {
+                InternalInsertNodeBefore(head, node);
+            }
+            node.list = this;
+        }
+
+        public bool Remove(T value)
+        {
+            LinkedListNode<T> node = Find(value);
+            if (node != null)
+            {
+                InternalRemoveNode(node);
+                return true;
+            }
+            return false;
+        }
+
+        public void Remove(LinkedListNode<T> node)
+        {
+            ValidateNode(node);
+            InternalRemoveNode(node);
+        }
+
+        public void RemoveFirst()
+        {
+            if (head == null) { throw new InvalidOperationException(SR.LinkedListEmpty); }
+            InternalRemoveNode(head);
+        }
+
+        public void RemoveLast()
+        {
+            if (head == null) { throw new InvalidOperationException(SR.LinkedListEmpty); }
+            InternalRemoveNode(head.prev);
+        }
+
+
+        private void InternalInsertNodeBefore(LinkedListNode<T> node, LinkedListNode<T> newNode)
+        {
+            newNode.next = node;
+            newNode.prev = node.prev;
+            node.prev.next = newNode;
+            node.prev = newNode;
+            version++;
+            count++;
+        }
+
+        private void InternalInsertNodeToEmptyList(LinkedListNode<T> newNode)
+        {
+            Debug.Assert(head == null && count == 0, "LinkedList must be empty when this method is called!");
+            newNode.next = newNode;
+            newNode.prev = newNode;
+            head = newNode;
+            version++;
+            count++;
+        }
+
+        internal void InternalRemoveNode(LinkedListNode<T> node)
+        {
+            Debug.Assert(node.list == this, "Deleting the node from another list!");
+            Debug.Assert(head != null, "This method shouldn't be called on empty list!");
+            if (node.next == node)
+            {
+                Debug.Assert(count == 1 && head == node, "this should only be true for a list with only one node");
+                head = null;
+            }
+            else
+            {
+                node.next.prev = node.prev;
+                node.prev.next = node.next;
+                if (head == node)
+                {
+                    head = node.next;
+                }
+            }
+            node.Invalidate();
+            count--;
+            version++;
+        }
+    }
+}
