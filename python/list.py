667bd354c3e86e39158e0f4cdd2e4bc5218f88b2
weizhixing <2591612720@qq.com>
2021-11-09 15:56:14 +0800

    b tree

---
+++ python/list.py
@@ -0,0 +1,88 @@
+musicweblist=[
+    'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+]
+
+video_list = [
+    'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+]
+music_list = [
+    'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+]
+漫画列表 = [
+    'https://www.mangapark.com/manga/one-piece/',
+    'https://www.manhuagui.com/comic/11/',
+    'https://www.comic-walker.com/',
+]
+
+job_list = [
+    'https://www.zhipin.com',
+    'https://www.lagou.com',
+    'https://www.51job.com',
+    'https://www.liepin.com',
+    'https://www.zhaopin.com',
+    'https://www.douban.com',
+    'https://www.douban.com',
+    'https://linkding.com',
+    'https://www.stackoverflow.com',
+    'https://www.zhihu.com',
+    'https://www.jianshu.com',
+    'https://www.csdn.net',
+    'https://www.cnblogs.com',
+    'https://www.oschina.net',
+    'https://www.jikexueyuan.com',
+    'https://www.imooc.com',
+    'https://www.w3school.com.cn',
+    'https://www.runoob.com',
+    'https://www.cnblogs.com',
+]
+learn_list = [
+    'https://www.zhihu.com',
+    'https://www.jianshu.com',
+    'https://www.csdn.net',
+    'https://www.cnblogs.com',
+    'https://www.oschina.net',
+    'https://www.jikexueyuan.com',
+    'https://www.imooc.com',
+    'https://www.w3school.com.cn',
+    'https://www.runoob.com',
+]
+video_list = [
+    'https://www.youtube.com',
+    'https://www.bilibili.com',
+    'https://www.douyu.com',
+    'https://www.huya.com',
+    'https://www.panda.tv',
+    'https://www.quanmin.tv',
+    'https://www.longzhu.com',
+    'https://www.huomao.com',
+    'https://www.acfun.cn',
+    'https://www.lives.acfun.cn',
+    'https://www.zhanqi.tv',
+]
+movie_list = [
+    'https://www.imdb.com',
+    'https://www.rottentomatoes.com',
+    'https://www.douban.com',
+]
+book_list = [
+    'https://www.douban.com',
+    'https://www.amazon.com',
+    'https://www.jd.com',
+    'https://www.taobao.com',
+]
+code_review_list = [
+    'https://www.github.com',
+    'https://www.gitlab.com',
+    'https://www.gitee.com',
+    'https://www.coding.net',
+    'https://www.gitbook.com',
+    'https://www.gitbook.io',
+]
+social_list = [
+    'https://www.weibo.com',
+    'https://www.zhihu.com',
+    'https://www.douban.com',
+]
+
