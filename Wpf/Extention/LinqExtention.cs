40a283e37ae99e36f3e212fdf24e0cfd4d96cccf
weizhixing <2591612720@qq.com>
2021-11-10 20:07:17 +0800

    japan learning

---
+++ Wpf/Extention/LinqExtention.cs
@@ -0,0 +1,11 @@
+public statcic class LinqExtenction{
+    public statcic T Find<T>(this IEnumerable<T> source,string configFile, Func<T,bool> predicate == null){
+        var item = source.<T>().FirstOrDefault(predicate);
+        if(item == null){
+            //throw new Exception($"{nameof(T)} not found in {configFile}!");
+            messageBox.Show($"{nameof(T)} not found in {configFile}!");
+            return null;
+        }
+        return item;
+    }
+}
