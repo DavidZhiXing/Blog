e59c271eb36d9cf3ee83f21f5010bafe3422f2f4
weizhixing <2591612720@qq.com>
2021-12-08 16:12:22 +0800

    read log file

--- python/language.py
+++ python/language.py
@@ -3,24 +3,49 @@ Chinese = [
     '今天天气真好',
     '我喜欢听歌',
     '你有什么好的建议吗',
-    '你叫什么名字'
+    '你叫什么名字',
+    '黑客与画家',
+    '太强了',
+    '乐迪加速',
+    '马蜂是从哪里来的',
+    '消防员在那里干什么',
+    '好大的消防车啊',
+    
 ]
 
 Japanese = [
     '今日はいい天気です',
     '私は歌を聴きます',
     'あなたは何か好きな建言ですか',
-    'あなたの名前は何ですか'
+    'あなたの名前は何ですか',
+    'ハッカーと芸術家',
+    'おっと、そんなに強いです',
+    'レディースアップ',
+    'マーブルはどこからきましたか',
+    '消防員はどこで仕事をしていますか',
+    'おい、そんな大きな消防車ですね',
 ]
 English = [
     'Today is a good day',
     'I like to listen music',
     'Do you have any good suggestions?',
-    'What is your name'
+    'What is your name',
+    'Hacker and artist',
+    'That is too strong',
+    'LetiJia speed up',
+    'Hornet is from where',
+    'Firefighter is doing what',
+    'That is a big fire truck',
 ]
 German = [
     'Heute ist ein guter Tag',
     'Ich höre Musik',
     'Haben Sie irgendwelche gute Vorschläge?',
-    'Wie heißt du'
+    'Wie heißt du',
+    'Hacker und Künstler',
+    'Das ist zu stark',
+    'LetiJia beschleunigen',
+    'Hornet ist von wo kommt',
+    'Feuerwehr ist was machen',
+    'Das ist ein großer Feuerwagen',
 ]
